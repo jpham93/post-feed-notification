712b4551b02100e363c619af4c620c9bd94d26e3
James Pham <jamespham93@yahoo.com>
2021-05-20 14:48:45 -0700

    Init commit

---
+++ post-feed-notification.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name:    Post Feed Notification
+Plugin URI:     https://github.com/jpham93/post-feed-notification
+Description:    A basic plugin that provides a dashboard feed of the newest post in a specified category. Also emails users of newly created posts.
+Author:         James Pham
+Version:        1.0
+Author URI:     https://jamespham.io
+*/
+
