55584d5eb1acc65d788c040b73908def8937fc7d
James Pham <jamespham93@yahoo.com>
2021-05-20 17:19:12 -0700

    Create validation checks in POST handler

--- post-feed-notification.php
+++ post-feed-notification.php
@@ -21,9 +21,15 @@ class PostFeedNotification {
     function __construct() {
 
         add_action( 'admin_menu', [ $this, 'register_settings_page' ], 10, 0 );
+        add_action( 'admin_post_create_new_feed', [ $this, 'create_new_feed' ], 10, 0 );
+        add_action( 'admin_notices', [$this, 'settings_page_notifications'], 10, 0 );
 
     }
 
+    /**
+     * SUBMENU CALLBACK
+     * Registers the submenu page under WP's core "Settings" tab.
+     */
     public function register_settings_page() {
 
         add_submenu_page(
@@ -37,9 +43,84 @@ class PostFeedNotification {
 
     }
 
+    /**
+     * FRONT END SETTINGS PAGE
+     * HTML for admin access.
+     */
     public function settings_page() {
 
         require_once 'views/settings-page.php';
+
+    }
+
+    /**
+     * POST ENDPOINT FOR CREATING NEW FEED
+     * Form submission handler for creating new feed.
+     */
+    public function create_new_feed() {
+
+        $term_id = $_POST['term_id'] ?? null;
+
+        // CHECK IF VALID PAYLOAD
+        if ( empty($term_id) ) {
+
+            $_SESSION['error'] = 'Invalid selection.';
+            wp_redirect('/wp-admin/options-general.php?page=post-feed');
+            exit;
+
+        }
+
+        // CHECK IF WP_TERM EXISTS W/ ID
+        if ( !term_exists((int) $term_id) ) {
+
+            $_SESSION['error'] = 'Error. Category does not exist';
+            wp_redirect('/wp-admin/options-general.php?page=post-feed');
+            exit;
+
+        }
+
+        $feeds = get_option('pfn_dashboard_feeds');
+
+        // CREATE NEW ARRAY OF FEED IF OPTION DOES NOT EXIST YET
+        if ( !$feeds ) {
+            add_option( 'pfn_dashboard_feeds', array( $term_id ) );
+
+        } elseif ( in_array($term_id, $feeds) ) {
+
+            $wp_term            = get_term($term_id);
+            $_SESSION['error']  = "Feed for {$wp_term->name} already exists. Please select another.";
+            wp_redirect('/wp-admin/options-general.php?page=post-feed');
+            exit;
+
+        }
+
+    }
+
+    public function settings_page_notifications() {
+
+        // Display errors
+        if ( isset($_SESSION['error']) ) {
+            ?>
+
+            <div class="notice notice-error is-dismissible">
+                <p><?php echo $_SESSION['error'] ?></p>
+            </div>
+
+            <?php
+            unset($_SESSION['error']);
+        }
+
+        // Display success
+        if ( isset($_SESSION['success']) ) {
+            ?>
+
+            <div class="notice notice-success is-dismissible">
+                <p><?php echo $_SESSION['success'] ?></p>
+            </div>
+
+            <?php
+            unset($_SESSION['success']);
+        }
 
     }
 
--- views/settings-page.php
+++ views/settings-page.php
@@ -4,25 +4,56 @@
  * @author          James Pham
  * @description     Settings page HTML for Post Feed Notification plugin.
  */
+
+$feeds = get_option('pfn_dashboard_feeds');
+do_action('qm/debug', $feeds);
 ?>
+
 
 <h1>Settings Page</h1>
 
 <p class="description">
-    Please select users
+    Select a post category to display as a feed on the main dashboard.
 </p>
 
+<form action="<?php echo admin_url('admin-post.php') ?>" method="post">
+    <input type="hidden" name="action" value="create_new_feed">
+    <table>
+        <tr>
+            <td>
+                <label for="category-select">Select a new</label>
+            </td>
+            <td>
+                <select name="term_id" id="category-select">
+                    <option value="" disabled selected>Select a Category</option>
 
-<?php
-$args = array(
-    'taxonomy'   => 'category',
-    'hide_empty' => false
-);
-$post_categories = get_categories($args);
+                    <?php
+                    $args = array(
+                        'taxonomy'   => 'category',
+                        'hide_empty' => false
+                    );
 
-do_action('qm/debug', $post_categories);
-?>
+                    /**
+                     * strip "uncategorized" terms from the drop down
+                     * @var WP_Term[]
+                     */
+                    $post_categories = array_filter( get_categories( $args ), fn( WP_Term $wp_term ) => $wp_term->slug !== 'uncategorized' );
 
-<form action="">
+                    foreach ( $post_categories as $wp_term ) :
+                        ?>
+
+                        <option value="<?php echo $wp_term->term_id ; ?>">
+                            <?php echo $wp_term->name ; ?>
+                        </option>
+
+                    <?php endforeach; ?>
+
+                </select>
+            </td>
+            <td>
+                <button class="button" type="submit">Create Feed</button>
+            </td>
+        </tr>
+    </table>
 
 </form>
